483978e5293c2ce8904e075b5065151bcdeec89a
Bojidar53443 <enevdar@gmail.com>
2026-04-29 08:45:37 +0300

    Add project files.

---
+++ Travel-Agency-System 2.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Travel_Agency_System_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
