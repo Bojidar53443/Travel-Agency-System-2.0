53797d69c5bbe6fd997ef9ab122c6584ea286aae
Bozhidar Enev <enevbozhidar@gmail.com>
2026-05-25 11:56:59 +0300

    started adding sql connections

---
+++ Travel-Agency-System 2.0/sql connection/TravelAgencyDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Travel_Agency_System_2._0.Models;
+
+namespace Travel_Agency_System_2._0.sql_connection
+{
+    public class TravelAgencyDbContext : DbContext
+    {
+        public TravelAgencyDbContext(DbContextOptions<TravelAgencyDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=YOUR_SERVER_NAME;Database=TravelAgencyDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
+        }
+    }
+}
